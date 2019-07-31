5c2faa4bf56891d1f79ddd63c289fe750be41c0f
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-07-31 18:39:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["552153150883233793"]
+const developers = ["595653353783033857"]
 const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
